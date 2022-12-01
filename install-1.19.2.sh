43df05ba74f8e968722d1080499c894e9d2d3e69
Riley Webb <web0018@elthamhs.vic.edu.au>
2022-12-01 23:06:21 +1100

    first

--- install-1.19.2.sh
+++ install-1.19.2.sh
@@ -4,17 +4,15 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt-get install wget gnupg openjdk-17-jre -y
 
-mkdir ~/paper-1.19.2
-cd ~/paper-1.19.2
+mkdir /usr/share/mc-server/paper-1.19.2
 
 LATEST_BUILD=$(curl -X GET "https://papermc.io/api/v2/projects/paper/versions/1.19.2" -H  "accept: application/json" | jq '.builds[-1]')
 
-curl -o paperclip.jar -X GET "https://papermc.io/api/v2/projects/paper/versions/1.19.2/builds/${LATEST_BUILD}/downloads/paper-1.19.2-${LATEST_BUILD}.jar" -H  "accept: application/java-archive" -JO
+curl -o /usr/share/mc-server/paper-1.19.2/paperclip.jar -X GET "https://papermc.io/api/v2/projects/paper/versions/1.19.2/builds/${LATEST_BUILD}/downloads/paper-1.19.2-${LATEST_BUILD}.jar" -H  "accept: application/java-archive" -JO
 
-cp start-1.19.2.sh ~/paper-1.19.2
+cp start-1.19.2.sh /usr/share/mc-server/paper-1.19.2
 
-mkdir -p ~/.config/systemd/user/
-sudo cp mc-1.19.2.service ~/.config/systemd/user/
+sudo cp mc-1.19.2.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
-sudo systemctl enable --user --now mc-1.19.2.service
+sudo systemctl enable --now mc-1.19.2.service
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+```
+chmod +x install-1.19.2.sh
+chmod +x start-1.19.2.sh
+```
