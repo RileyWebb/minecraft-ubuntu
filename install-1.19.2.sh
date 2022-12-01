e15aea888f7edea92752a4789dbcf6eb5873939c
Riley Webb <web0018@elthamhs.vic.edu.au>
2022-12-01 23:23:13 +1100

    third

--- install-1.19.2.sh
+++ install-1.19.2.sh
@@ -2,9 +2,9 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt-get install wget gnupg openjdk-17-jre -y
+sudo apt-get install wget gnupg openjdk-17-jre screen -y
 
-mkdir /usr/share/mc-server/paper-1.19.2
+mkdir -p /usr/share/mc-server/paper-1.19.2
 
 LATEST_BUILD=$(curl -X GET "https://papermc.io/api/v2/projects/paper/versions/1.19.2" -H  "accept: application/json" | jq '.builds[-1]')
 
@@ -14,5 +14,8 @@ cp start-1.19.2.sh /usr/share/mc-server/paper-1.19.2
 
 sudo cp mc-1.19.2.service /etc/systemd/system/
 
+sudo echo "eula=true" >> /usr/share/mc-server/paper-1.19.2/eula.txt
+
 sudo systemctl daemon-reload
 sudo systemctl enable --now mc-1.19.2.service
+
